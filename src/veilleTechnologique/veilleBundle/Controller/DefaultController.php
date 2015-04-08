892b1aa02a87d52967dec659bbcea66a0dafa4db
Jérôme Pros <jerome.pros@gmail.com>
2015-04-08 12:58:58 +0200

    Voir commentaire (important)

--- src/veilleTechnologique/veilleBundle/Controller/DefaultController.php
+++ src/veilleTechnologique/veilleBundle/Controller/DefaultController.php
@@ -9,69 +9,59 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use veilleTechnologique\veilleBundle\Entity\User as User;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Controller ANONYMOUS. Functions :
+ * - Register
+ * - Login
+ */
+
 class DefaultController extends Controller
 {
-   
-    
     /**
-     * @Route("/", name="_home_hello")
+     * @Route("/", name="_default_hello")
      * @Template()
      */
     public function indexAction()
     {
-        /*$userSet = new User();
-        
-        $userSet->setLogin('ludo');
-        $userSet->setPass('popo');
-        $userSet->setEmail('ludo@popo.fr');
-        */
-        
-        $user=$this->getDoctrine()->getRepository("veilleTechnologiqueveilleBundle:User")->findOneBy(array('id' => '1'));
-        
-        /*
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($userSet);
-        $em->flush();
-      */
-        
-        
-        
         return array();
     }
     
     /**
-     * @Route("/inscription", name="_home_inscription")
+     * @Route("/register", name="_default_inscription")
      * @Template()
      */
     public function inscriptionAction(Request $request)
     {
+        // Création d'un objet utilisateur.
         $user = new User();
+        
+        // Création du formulaire.
         $form = $this->get('form.factory')->createBuilder('form', $user)
                 ->add('login','text')
                 ->add('pass','password')
                 ->add('email','text')
                 ->add('valide','submit')
-                ->getForm()
-        ;
+                ->getForm();
         
+        // Réception des données du formulaire.
         $form->handleRequest($request);
         
         if($form->isValid())
         {
+            // On persiste l'utilisateur dans la base de donnée.
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
             
-            $this->get('session')->getFlashBag()->add(
-            'notice',
-            'Vos changements ont été sauvegardés!');
-            
-            //return $this->render('veilleTechnologiqueveilleBundle::layout.html.twig',array());
-            return $this->redirect($this->generateUrl('_home_hello', array()));
-            
+            // Envois un message flash à l'utilisateur.
+            $this->get('session')->getFlashBag()->add('success','Vous vous êtes bien enregistré(e) sur l\'application, '.$user->getLogin().' !');
+
+            // On redirige l'utilisateur sur l'accueil.
+            return $this->redirect($this->generateUrl('_default_hello', array()));
         }
+        // TODO : Condition ELSE à faire : renvoyer un message d'erreur pour dire que c'est invalide.
         
-        
+        // On l'envoit sur l'inscription.
         return array('form' => $form->createView());
     }
 }
